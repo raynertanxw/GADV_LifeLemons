5098d2081247e943e9929ef29df46251577f5e30
raynertanxw <raynertanxw@me.com>
2015-06-19 12:55:04 +0800

    Enemy Movement States Implemented
    - Implemented the three movement behavior states: follow, strafing, and
    circle strafing.
    - Put in and tested default values for normalhench and mediumhench for
    all three states.
    
    - Split up the sprite sheet for meleehench.

--- Life Lemons/Assets/Scripts/Enemies/Enemy_Movement.cs
+++ Life Lemons/Assets/Scripts/Enemies/Enemy_Movement.cs
@@ -1,10 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum EnemyMovementStates {follow, strafing, circleStrafing};
+
 public class Enemy_Movement : MonoBehaviour
 {
-	public float movementSpeed;
-	public float minDistanceFromPlayer;
+	public EnemyMovementStates movementState;
+
+	// follow state variables.
+	public float followMovementSpeed;
+	public float followMinDistanceFromPlayer;
+
+	// strafing state variables.
+	public float strafingMovementSpeed;
+	public int strafingWaypointIndex;
+	public float strafingWaypointTolerence; // The maximum distance to the current waypoint before switching waypoints.
+	public Vector3[] strafingWaypoints;
+
+	// circleStrafing state variables.
+	public float circleStrafingRadius;
+	public float circleStrafingMovementSpeed;
+	public float circleStrafingTargetTolerence; // The maximum distance to the current targetVector.
+	public float circleStrafingTargetIncrement; // Increment of target angle (i.e. how fast the target position moves around the player).
+	public float circleStrafingTargetAngle; // The target angle of rotation used to calculate the target vector of the circle around the player.
+	public Vector3 circleStrafingTargetVector; // The target vector of the circle around the player.
 	
 	private Vector3 directionVec;
 	private GameObject player; // Reference to the player.
@@ -17,10 +36,54 @@ public class Enemy_Movement : MonoBehaviour
 	void Update()
 	{
 		// Handling movement of the enemy.
-		directionVec = player.transform.position - transform.position;
-		if (directionVec.magnitude > minDistanceFromPlayer)
+		switch (movementState)
 		{
-			transform.position += directionVec * movementSpeed * Time.deltaTime;
+		case EnemyMovementStates.follow:
+			directionVec = player.transform.position - transform.position;
+			if (directionVec.magnitude > followMinDistanceFromPlayer)
+			{
+				transform.position += directionVec.normalized * followMovementSpeed * Time.deltaTime;
+			}
+			break;
+
+		case EnemyMovementStates.strafing:
+			directionVec = strafingWaypoints[strafingWaypointIndex] - transform.position;
+			if (directionVec.magnitude < strafingWaypointTolerence)
+			{
+				// Switch waypoint.
+				strafingWaypointIndex++;
+				if (strafingWaypointIndex == strafingWaypoints.Length)
+				{
+					strafingWaypointIndex = 0; // Reset the waypoint index.
+				}
+			}
+			else
+			{
+				transform.position += directionVec.normalized * strafingMovementSpeed * Time.deltaTime;
+			}
+
+			directionVec = player.transform.position - transform.position; // For facing the player.
+			break;
+
+		case EnemyMovementStates.circleStrafing:
+			// Calcuate the circleStrafingTargetVector.
+			circleStrafingTargetAngle = (circleStrafingTargetAngle + circleStrafingTargetIncrement) % 360;
+			circleStrafingTargetVector = Utilities.RotationZ2DirectionVec(circleStrafingTargetAngle) * circleStrafingRadius;
+			circleStrafingTargetVector = player.transform.position + circleStrafingTargetVector;
+
+			directionVec = circleStrafingTargetVector - transform.position;
+			if (directionVec.magnitude > circleStrafingTargetTolerence)
+			{
+				// Move.
+				transform.position += directionVec.normalized * circleStrafingMovementSpeed * Time.deltaTime;
+			}
+
+			directionVec = player.transform.position - transform.position; // For facing the player.
+			break;
+
+		default:
+			Debug.Log("ERROR: EnemyMovementStates out of range");
+			break;
 		}
 		
 		// Handle rotation of the enemy.
