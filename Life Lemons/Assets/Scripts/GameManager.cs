02f30ef9e566bd98c0a56a45ac8533be6e6d16db
raynertanxw <raynertanxw@me.com>
2015-06-24 01:03:07 +0800

    Error Catching for txt read instructions
    - GameManager.cs coroutine now checks with extensive if-else, try-catch
    blocks for invalid syntax such as missing numbers, invalid numbers,
    wrong number of parameters, etc.
    
    Tested and should be able to prevent any runtime exceptions.

--- Life Lemons/Assets/Scripts/GameManager.cs
+++ Life Lemons/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -72,41 +73,178 @@ public class GameManager : MonoBehaviour
 				instructuion[i] = instructuion[i].TrimStart(' ');
 			}
 
+			bool legalSyntax = true;
+
+			// Check if delay syntax is correct first.
+			try
+			{
+				float.Parse(instructuion[instructuion.Length-1]);
+			}
+			catch (FormatException fe)
+			{
+				legalSyntax = false;
+				Debug.Log(fe.Message + "\n found in instruction Line: " + (currentInstructuion + 1) + " for delay parameter");
+			}
+
 			switch (instructuion[0])
 			{
 			case "SpawnFollow":
-				spawner.SpawnFollow(int.Parse(instructuion[1]), int.Parse(instructuion[2]));
+
+				// Check for any invalid parameters.
+				try
+				{
+					int.Parse(instructuion[1]);
+					int.Parse(instructuion[2]);
+				}
+				catch (FormatException fe)
+				{
+					legalSyntax = false;
+					Debug.Log(fe.Message + "\n found in instruction Line: " + (currentInstructuion + 1) + " for int/float typed parameter");
+					break;
+				}
+
+				if (instructuion.Length != 4) // Must be 4 parameters.
+					legalSyntax = false;
+				else if (int.Parse(instructuion[1]) < 0 || int.Parse(instructuion[1]) > 2) // Enemy type int out of index range.
+					legalSyntax = false;
+				else if (int.Parse(instructuion[2]) < 0 || int.Parse(instructuion[2]) > 15) // Spawnpoint int out of index range.
+					legalSyntax = false;
+				// Else, safe to execute.
+				else
+				{
+					spawner.SpawnFollow(int.Parse(instructuion[1]), int.Parse(instructuion[2]));
+				}
 				break;
 
 			case "SpawnStrafe":
-				// Convert values at index 3 to second before end of instruction array into array of Vector3s.
-				int numOfWaypoints = (instructuion.Length - 4) / 2;
-				Vector3[] waypoints = new Vector3[numOfWaypoints];
-				for (int i = 0; i < numOfWaypoints; i++) // i is the index of the waypoint.
+
+				// Check for first two invalid parameters.
+				try
 				{
-					waypoints[i] = new Vector3(float.Parse(instructuion[(i*2 + 3)].TrimStart('(')), float.Parse(instructuion[(i*2 + 4)].TrimEnd(')')), 0f);
+					int.Parse(instructuion[1]);
+					int.Parse(instructuion[2]);
 				}
-				spawner.SpawnStrafe(int.Parse(instructuion[1]), int.Parse(instructuion[2]), waypoints);
+				catch (FormatException fe)
+				{
+					legalSyntax = false;
+					Debug.Log(fe.Message + "\n found in instruction Line: " + (currentInstructuion + 1) + " for int/float typed parameter");
+					break;
+				}
+
+				if (instructuion.Length < 8) // Minimum of 2 waypoints for strafe behaviour to work.
+					legalSyntax = false;
+				else if (instructuion.Length % 2 != 0) // Due to waypoints having two values each, it will always be even unless a value is missing.
+					legalSyntax = false;
+				else if (int.Parse(instructuion[1]) < 0 || int.Parse(instructuion[1]) > 2) // Enemy type int out of index range.
+					legalSyntax = false;
+				else if (int.Parse(instructuion[2]) < 0 || int.Parse(instructuion[2]) > 15) // Spawnpoint int out of index range.
+					legalSyntax = false;
+				// Else, safe to execute.
+				else
+				{
+					// Convert values at index 3 to second before end of instruction array into array of Vector3s.
+					int numOfWaypoints = (instructuion.Length - 4) / 2;
+					Vector3[] waypoints = new Vector3[numOfWaypoints];
+
+					// Check if any of the vectors have illegal syntax.
+					try
+					{
+						for (int i = 0; i < numOfWaypoints; i++) // i is the index of the waypoint.
+						{
+							waypoints[i] = new Vector3(float.Parse(instructuion[(i*2 + 3)].TrimStart('(')), float.Parse(instructuion[(i*2 + 4)].TrimEnd(')')), 0f);
+						}
+					}
+					catch (FormatException fe)
+					{
+						legalSyntax = false;
+						Debug.Log(fe.Message + "\n found in instruction Line: " + (currentInstructuion + 1) + " for int/float typed parameter");
+						break;
+					}
+
+					spawner.SpawnStrafe(int.Parse(instructuion[1]), int.Parse(instructuion[2]), waypoints);
+				}
 				break;
 
 			case "SpawnQuadCircleStrafe":
+
 				// First Method Overload variant.
 				if (instructuion.Length == 3)
 				{
-					spawner.SpawnQuadCircleStrafe(int.Parse(instructuion[1]));
+					// Check for any invalid parameters.
+					try
+					{
+						int.Parse(instructuion[1]);
+					}
+					catch (FormatException fe)
+					{
+						legalSyntax = false;
+						Debug.Log(fe.Message + "\n found in instruction Line: " + (currentInstructuion + 1) + " for int/float typed parameter");
+						break;
+					}
+
+					if (int.Parse(instructuion[1]) < 0 || int.Parse(instructuion[1]) > 2) // Enemy type int out of index range.
+						legalSyntax = false;
+					// Else, safe to execute.
+					else
+					{
+						spawner.SpawnQuadCircleStrafe(int.Parse(instructuion[1]));
+					}
 				}
 				// Second Method Overload variant, where the individual types are specified.
+				else if (instructuion.Length == 6)
+				{
+					// Check for any invalid parameters.
+					try
+					{
+						int.Parse(instructuion[1]);
+						int.Parse(instructuion[2]);
+						int.Parse(instructuion[3]);
+						int.Parse(instructuion[4]);
+					}
+					catch (FormatException fe)
+					{
+						legalSyntax = false;
+						Debug.Log(fe.Message + "\n found in instruction Line: " + (currentInstructuion + 1) + " for int/float typed parameter");
+						break;
+					}
+
+					// Check for out of index range enemy type.
+					for (int i = 1; i < 5; i++)
+					{
+						if (int.Parse(instructuion[i]) < 0 || int.Parse(instructuion[i]) > 2)
+						{
+							legalSyntax = false;
+							break;
+						}
+					}
+					// If still legal syntax, then safe to execute.
+					if (legalSyntax == true)
+					{
+						spawner.SpawnQuadCircleStrafe(int.Parse(instructuion[1]), int.Parse(instructuion[2]), int.Parse(instructuion[3]), int.Parse(instructuion[4]));
+					}
+				}
+				// Otherwise, illegal syntax.
 				else
 				{
-					spawner.SpawnQuadCircleStrafe(int.Parse(instructuion[1]), int.Parse(instructuion[2]), int.Parse(instructuion[3]), int.Parse(instructuion[4]));
+					legalSyntax = false;
 				}
+
 				break;
 
 			default:
 				Debug.Log("Error: No such spawn instruction exists\nError in instruction line: " + (currentInstructuion + 1));
 				break;
 			}
-			yield return new WaitForSeconds(float.Parse(instructuion[instructuion.Length-1]));
+
+			// If the legalSyntax flag is false, state the line, and skip the delay, move on to the next wave by going to the next loop of this while loop.
+			if (legalSyntax == false)
+			{
+				Debug.Log("Illegal Spawn Syntax in instruction line: " + (currentInstructuion + 1));
+			}
+			else
+			{
+				yield return new WaitForSeconds(float.Parse(instructuion[instructuion.Length-1]));
+			}
 		}
 	}
 }
