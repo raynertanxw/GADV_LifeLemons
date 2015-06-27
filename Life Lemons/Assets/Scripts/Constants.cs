5bf253b716e4a5e81d59e0d0446fe0e2fba0fa7c
raynertanxw <raynertanxw@me.com>
2015-06-27 19:56:22 +0800

    New Levels and Heart positioning
    - Added levels 3-5
    - Edited level 1
    - Updated constants to new numOfLevels
    - Positioned the 10 health hearts in level scene.

--- Life Lemons/Assets/Scripts/Constants.cs
+++ Life Lemons/Assets/Scripts/Constants.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public static class Constants
 {
-	public static int NumOfLevels = 2;
+	public static int NumOfLevels = 5;
 
 	// Save data strings.
 	public static string HIGHEST_CLEARED_LEVEL = "HIGHEST_CLEARED_LEVEL";
