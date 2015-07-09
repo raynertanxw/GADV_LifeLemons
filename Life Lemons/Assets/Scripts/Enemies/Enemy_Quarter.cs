d8f4264f1d2e80d0e571dce7815cd033404f1e49
raynertanxw <raynertanxw@me.com>
2015-07-10 00:57:50 +0800

    Quarters Prefab and script implemented
    - Created Quarters prefab
    - Created Quarters behaviour script
    - New sorting layer: Collectables
    
    NOTE: NOT TESTED YET.

---
+++ Life Lemons/Assets/Scripts/Enemies/Enemy_Quarter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Quarter : MonoBehaviour
+{
+	private SpriteRenderer spriteRen;
+
+	void Awake()
+	{
+		spriteRen = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void Start()
+	{
+		StartCoroutine(FlashAndDespawn());
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == Constants.tagPlayer)
+		{
+			// Add to total quarter count.
+			if (PlayerPrefs.HasKey(Constants.NUM_OF_QUARTERS) == true)
+			{
+				PlayerPrefs.SetInt(Constants.NUM_OF_QUARTERS, PlayerPrefs.GetInt(Constants.NUM_OF_QUARTERS) + 1);
+			}
+			else
+			{
+				PlayerPrefs.SetInt(Constants.NUM_OF_QUARTERS, 1);
+			}
+			Destroy(gameObject); // Despawn self.
+		}
+	}
+
+	IEnumerator FlashAndDespawn()
+	{
+		Color orgColor = spriteRen.color;
+
+		yield return new WaitForSeconds(5.0f);
+
+		for (int i = 0; i < 5; i++)
+		{
+			orgColor.a = 0.0f;
+			spriteRen.color = orgColor;
+			yield return new WaitForSeconds(0.2f);
+			orgColor.a = 1.0f;
+			spriteRen.color = orgColor;
+			yield return new WaitForSeconds(0.8f);
+		}
+
+		Destroy(gameObject); // Despawn self.
+	}
+}
