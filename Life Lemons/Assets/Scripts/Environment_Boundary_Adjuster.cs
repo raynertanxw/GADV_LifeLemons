8675e32c0d79f292f70884bcd7d54aa412eb2993
raynertanxw <raynertanxw@me.com>
2015-07-21 15:19:07 +0800

    UI now fades slightly when player is under it.
    - Boundary Adjuster now adjusts colliders that are positioned under the
    UI elements.
    - New script UIFaceController detects when the player enters the
    respective collider and fades away the respective UI element until it
    is 0.5 alpha. When player exist, the same script will fade the Ui
    element back to 1.0 alpha.

--- Life Lemons/Assets/Scripts/Environment_Boundary_Adjuster.cs
+++ Life Lemons/Assets/Scripts/Environment_Boundary_Adjuster.cs
@@ -6,6 +6,7 @@ public class Environment_Boundary_Adjuster : MonoBehaviour
 	private float minX, maxX, minY, maxY;
 	private float boundaryThickness = 1.0f;
 	public BoxCollider2D boundaryTop, boundaryRight, boundaryBottom, boundaryLeft;
+	public BoxCollider2D UITrigger_AmmoLevelIndicator, UITrigger_Hearts;
 	
 	void Start()
 	{
@@ -29,5 +30,21 @@ public class Environment_Boundary_Adjuster : MonoBehaviour
 
 		boundaryLeft.offset = new Vector2(minX - boundaryThickness/2.0f, 0f);
 		boundaryLeft.size = new Vector2(boundaryThickness, maxY - minY);
+
+
+		// Set up UITriggers
+		
+		// AmmoLevelIndicator is size 75x75 pivot at -55 -55 from top right.
+		float pixelToWorldSpaceEquivilent = 1f/800f * (maxX - minX);
+		UITrigger_AmmoLevelIndicator.offset = new Vector2(maxX - (55f * pixelToWorldSpaceEquivilent), maxY - (55f * pixelToWorldSpaceEquivilent));
+		UITrigger_AmmoLevelIndicator.size = new Vector2(75f * pixelToWorldSpaceEquivilent, 75f * pixelToWorldSpaceEquivilent);
+
+		// Hearts is size 30x30 and first heart pivot is 30x from left and -30y from top
+		// Hearts are spaced 5 pixels apart.
+		int numHearts = 5 + PlayerPrefs.GetInt(Constants.UPGRADE_DEFENSE_HEALTH);
+		float heartSizeX = numHearts * (30f * pixelToWorldSpaceEquivilent) + ((numHearts-1) * (5f * pixelToWorldSpaceEquivilent));
+		float heartOffsetX = (heartSizeX / 2.0f) + (30f * pixelToWorldSpaceEquivilent);
+		UITrigger_Hearts.offset = new Vector2(minX + heartOffsetX, maxY - (30f * pixelToWorldSpaceEquivilent));
+		UITrigger_Hearts.size = new Vector2(heartSizeX, 30f * pixelToWorldSpaceEquivilent);
 	}
 }
---
+++ Life Lemons/Assets/Scripts/UserInterface/UIFadeController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIFadeController : MonoBehaviour
+{
+	public CanvasGroup UIElementCG;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == Constants.tagPlayer)
+		{
+			// Fade to 0.5 alpha.
+			UIElementCG.alpha = 0.5f;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.tag == Constants.tagPlayer)
+		{
+			// There is only one player so don't need to count, just fade out.
+			// Fade to 1.0 alpha.
+			UIElementCG.alpha = 1.0f;
+		}
+	}
+	
+}
