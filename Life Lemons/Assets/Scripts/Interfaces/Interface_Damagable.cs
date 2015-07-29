89c2cb15a68e2aa7f53b47d6fee6f7ce30351d61
raynertanxw <raynertanxw@me.com>
2015-07-29 21:32:13 +0800

    Tidied up comments for Interface: IDamagable

--- Life Lemons/Assets/Scripts/Interfaces/Interface_Damagable.cs
+++ Life Lemons/Assets/Scripts/Interfaces/Interface_Damagable.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public interface IDamagable
 {
-	void TakeDamage(int damage);
-	void CheckGameOver();
+	void TakeDamage(int damage); // Common TakeDamage Function for projectiles to call.
+	void CheckGameOver(); // Common check to see if IDamagable object has died.
+	// (SPELLING MISTAKE SHOULD BE CheckDead). Not changing in fear of breaking code this near to deadline.
 }
